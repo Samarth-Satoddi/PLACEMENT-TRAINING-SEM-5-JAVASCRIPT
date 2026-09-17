b93c61013cfcb1e2c6179fdb4b96be0dd71379f1
Samarth Satoddi <samarthsatoddi@gmail.com>
2026-09-17 16:12:14 +0530

    learning design

---
+++ react-demo/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+function App() {
+  const trainername="SAMARTH SATODDI";
+  const trainingday=10;
+  return (
+    <main className="App">
+      <section className="App-header">
+      <p className="daylabel">React day</p>
+      <br></br>
+      <h2 className="title">College course explore </h2>
+      <p>Trainer:{trainername}</p>
+      <p>Training Day:{trainingday}</p>
+      <p>used PYTHON,REACT,LANGCHAIN</p>
+    </section>
+    </main>SOLVE THIS ALSO
+    
+  );
+} 
+export default App;
