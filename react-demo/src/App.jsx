46a48563fcf68eeb1d2b60817475a9a2b2adb397
Samarth Satoddi <samarthsatoddi@gmail.com>
2026-09-18 12:01:10 +0530

    Add hero and navbar React UI updates

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -1,19 +1,23 @@
 import "./App.css";
+import Hero from "./assets/component/hero";
+
 function App() {
-  const trainername="SAMARTH SATODDI";
-  const trainingday=10;
+  const trainername = "SAMARTH SATODDI";
+  const trainingday = 10;
+
   return (
     <main className="App">
+      <Hero />
       <section className="App-header">
-      <p className="daylabel">React day</p>
-      <br></br>
-      <h2 className="title">College course explore </h2>
-      <p>Trainer:{trainername}</p>
-      <p>Training Day:{trainingday}</p>
-      <p>used PYTHON,REACT,LANGCHAIN</p>
-    </section>
-    </main>SOLVE THIS ALSO
-    
+        <p className="daylabel">React day</p>
+        <br />
+        <h2 className="title">College course explore</h2>
+        <p>Trainer: {trainername}</p>
+        <p>Training Day: {trainingday}</p>
+        <p>used PYTHON, REACT, LANGCHAIN</p>
+      </section>
+    </main>
   );
-} 
+}
+
 export default App;
